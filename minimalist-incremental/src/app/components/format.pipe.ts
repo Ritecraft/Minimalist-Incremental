82ba2fa15bd5d01948e8ffefed5330426c303136
patryk.kujawski93@gmail.com <patryk.kujawski93@gmail.com>
2021-01-06 00:41:23 +0100

    removed unnecessary comment

--- minimalist-incremental/src/app/components/format.pipe.ts
+++ minimalist-incremental/src/app/components/format.pipe.ts
@@ -1,13 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 | exponentialStrength:10 }}
- *   formats to: 1024
-*/
 
 export class StringFormatter {
     public static Format(value: string, ...args: any[]) : string
